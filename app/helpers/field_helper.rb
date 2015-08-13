d5ba5940c0c53aa343875ed0937d09ffa3b14b1c
Carl Maxwell <carl.maxwell@gmail.com>
2015-08-13 08:33:34 -0700

    working on a helper for forms#show

---
+++ app/helpers/field_helper.rb
@@ -0,0 +1,23 @@
+module FieldHelper
+  def types
+    [
+      "text",
+      "textarea",
+      "dropdown",
+      "radio",
+      "checkbox",
+      "date",
+      "What you see is what you get editor (WYSIWYG)",
+      "Section break",
+      "page break",
+      "rating",
+      "address",
+      "website",
+      "phone",
+      "email",
+      "likert",
+      "code editor",
+      "Markup (commonmark) editor"
+    ]
+  end
+end
