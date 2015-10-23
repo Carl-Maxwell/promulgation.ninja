92087eb68fbef569d88a744d3648471cd17cac3a
Carl Maxwell <carl.maxwell@gmail.com>
2015-10-23 15:23:15 -0700

    properly hide navbar unless logged in viewing form
    * hide navbar if not logged in
    * hide navbar if not viewing form
    * fix 'all forms' link to link to '#'

--- app/assets/javascripts/views/navbar.js
+++ app/assets/javascripts/views/navbar.js
@@ -9,13 +9,25 @@ Promulgation.Views.Navbar = Backbone.View.extend({
 
   routerRouted: function(route, params) {
     var activePage = ['formEdit', 'formShow', 'submissionIndex'].indexOf(route);
-    if (activePage != -1) {
+    if (!isGuest() && activePage != -1) {
       this.render(params[0], activePage);
+    } else {
+      this.unrender();
     }
   },
 
   render: function(id, activePage) {
     this.$el.html(this.template({id: id, activePage: activePage}));
+
+    this.$el.removeClass('borderless');
+
+    return this;
+  },
+
+  unrender: function() {
+    this.$el.html('');
+
+    this.$el.addClass('borderless');
 
     return this;
   }
