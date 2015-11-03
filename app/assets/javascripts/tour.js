39347847d3971e630df5c4570cdf276438ebd7ef
Carl Maxwell <carl.maxwell@gmail.com>
2015-11-03 13:53:03 -0800

    add tour steps walking through submissions &c
    tour submissions, then go make a form.

--- app/assets/javascripts/tour.js
+++ app/assets/javascripts/tour.js
@@ -48,6 +48,18 @@
       return $('meta[name="is-logged-in-guest"]').length;
     };
 
+    var isModalOpen = function() {
+      return $('.modal:visible').length;
+    };
+
+    var isMirkwoodMarked = function() {
+      return shepherd.markedSteps.indexOf('close-mirkwood-submission') != -1;
+    };
+
+    var isOnFormsIndex = function() {
+      return location.hash == "";
+    };
+
     //
     // the steps
     //
@@ -65,17 +77,43 @@
         words: 'Click here to see what people are saying about the Council of Elrond\'s last meeting',
         attachTo: '[href="#/forms/10/submissions"] left',
         conditional: function(callback) {
-          if (isLoggedInGuest()) {
+          if (!isMirkwoodMarked() && isLoggedInGuest()) {
             callback();
           }
         }
       },
       {
-        id: 'mirkwood-submission',
+        id: 'open-mirkwood-submission',
         words: 'Lets see what the representative from Mirkwood has to say about the meeting!',
         attachTo: '[data-column-number="1"][href="#/submissions/6/"] top',
         conditional: function(callback) {
-          if (isLoggedInGuest()) {
+          if (!isModalOpen() && isLoggedInGuest()) {
+            callback();
+          }
+        }
+      },
+      {
+        id: 'close-mirkwood-submission',
+        words: 'Doesn\'t seem very happy. Well, it\'s something to take note of for the next meeting I guess... Let\'s move on!',
+        attachTo: '.good-button.close-modal top',
+        conditional: function(callback) {
+          if (isModalOpen() && isLoggedInGuest()) {
+            callback();
+          }
+        },
+        initialize: function() {
+          $('.good-button.close-modal').one('click', function() {
+            shepherd.markStep(this.id);
+            shepherd.addSteps();
+          }.bind(this));
+        }
+      },
+      {
+        id: 'goto-forms-index-to-add-form',
+        words: 'Now let\'s go add a new form of our own!',
+        attachTo: '.forms-index-link bottom',
+        conditional: function(callback) {
+          if (!isOnFormsIndex() && isMirkwoodMarked() && isLoggedInGuest()) {
             callback();
           }
         }
@@ -85,7 +123,7 @@
         words: 'Let\'s add a new form!',
         attachTo: '.add-form left',
         conditional: function(callback) {
-          if (isLoggedInGuest()) {
+          if (isMirkwoodMarked() && isLoggedInGuest()) {
             callback();
           }
         }
@@ -165,8 +203,9 @@
           selector = step.attachTo.split(' ').slice(0, -1).join(' ');
         }
 
-
         if ($(selector).length) {
+          if (!tour.steps.length && step.initialize) step.initialize();
+
           tour.addStep(step.id, {
             text: step.words,
             attachTo: step.attachTo,
